729907ac21660f40bdb2055ea87c79a95eb374f3
dpwrussell <douglas_russell@hms.harvard.edu>
2018-07-05 13:48:41 -0400

    Storage enhancements
    Add storage level to repository configuration
    Remove explicit key reference from images
    Add set_bfu_complete API endpoint

--- src/minerva_db/sql/models/image.py
+++ src/minerva_db/sql/models/image.py
@@ -7,15 +7,13 @@ from .bfu import BFU
 class Image(Base):
     uuid = Column(String(36), primary_key=True)
     name = Column(String(256), nullable=False)
-    key = Column(String(1024), nullable=False)
     pyramid_levels = Column(Integer, nullable=False)
     bfu_uuid = Column(String(36), ForeignKey(BFU.uuid), nullable=False)
 
     bfu = relationship('BFU', back_populates='images')
 
-    def __init__(self, uuid, name, key, pyramid_levels, bfu):
+    def __init__(self, uuid, name, pyramid_levels, bfu):
         self.uuid = uuid
         self.name = name
-        self.key = key
         self.pyramid_levels = pyramid_levels
         self.bfu = bfu
