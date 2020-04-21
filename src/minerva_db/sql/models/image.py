efd9f8e7c09a9fcb604ad0295af0d4b79d46d882
Juha Ruokonen <juha_ruokonen@hms.harvard.edu>
2020-04-21 09:14:34 -0400

    Add format, compression, tile_size columns to image.

--- src/minerva_db/sql/models/image.py
+++ src/minerva_db/sql/models/image.py
@@ -12,16 +12,22 @@ class Image(Base):
     deleted = Column(Boolean, nullable=False)
     fileset_uuid = Column(String(36), ForeignKey(Fileset.uuid), nullable=True)
     repository_uuid = Column(String(36), ForeignKey(Repository.uuid), nullable=True)
+    format = Column(String(256), nullable=True)
+    compression = Column(String(256), nullable=True)
+    tile_size = Column(Integer, nullable=False)
 
     fileset = relationship('Fileset', back_populates='images')
     repository = relationship('Repository', back_populates='images')
     rendering_settings = relationship('RenderingSettings', back_populates='image',
                           cascade='all, delete-orphan')
 
-    def __init__(self, uuid, name, pyramid_levels, fileset, repository):
+    def __init__(self, uuid, name, pyramid_levels, format, compression, tile_size, fileset, repository):
         self.uuid = uuid
         self.name = name
         self.pyramid_levels = pyramid_levels
         self.fileset = fileset
         self.repository = repository
         self.deleted = False
+        self.format = format
+        self.compression = compression
+        self.tile_size = tile_size
